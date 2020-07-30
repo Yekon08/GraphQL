483512e0f7f94927a823369a96d71ab1e773e6c0
Yekon08 <max1901@live.fr>
2020-07-30 15:50:08 +0200

    Book List with graphQL

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -13,13 +13,26 @@ const BOOK_LIST = gql`
 const BookList = () => {
 
     const { loading, error, data } = useQuery(BOOK_LIST)
-
     console.log('Test Data: ', data)
+
+    console.log('loading: ', loading)
+    if(loading) return <p>Loading...</p>
+
+    console.log('error: ', error)
+    if(error) return <p>Something Wrong</p>
+
+    console.log("data books : ", data.books)
+
+    const displayBooks = data.books.map(book => {
+        return(
+            <li key={book.id}>{book.name}</li>
+        )
+    })
 
     return (
         <>
             <ul id="book-list">
-                <li>Book name</li>
+                {displayBooks}
             </ul>
         </>
     )
