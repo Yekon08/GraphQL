70e87ddb35198705cf8bfe8e5cf5aa49cc8c00df
Yekon08 <max1901@live.fr>
2020-07-31 10:53:47 +0200

    Handle Form ADDBOOK

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { useQuery } from '@apollo/client'
 import { ADD_BOOK } from '../queries/queries'
 
 
+const formInputs = {
+    name: '',
+    genre: '',
+    authorId: ''
+}
+
+const reducerForm = (state, { field, value }) => {
+    return {
+        ...state,
+        [field] : value
+    }
+}
+
 const AddBook = () => {
 
     const { loading, error, data } = useQuery(ADD_BOOK)
+    const [formInputsValues, dispatch] = useReducer(reducerForm, formInputs)
 
     if(loading) return <p>Loading...</p>
     if(error) return <p>Something Wrong</p>
@@ -16,25 +30,33 @@ const AddBook = () => {
         )
     })
 
+    const handleInputChange = (e) => {
+        dispatch({ field: e.target.name, value: e.target.value })
+        // console.log('form: ', formInputsValues)
+    }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+        console.log('data: ', formInputsValues)
+    }
+
     return(
         <>
             <h1>Authors:</h1>
-
-            <form id="add-book">
+            <form id="add-book" onSubmit={handleSubmitForm}>
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" />
+                    <input type="text" name="name" value={formInputsValues.name} onChange={handleInputChange} />
                 </div>
 
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" />
+                    <input type="text" name="genre" value={formInputsValues.genre} onChange={handleInputChange} />
                 </div>
 
                 <div className="field">
                     <label>Author:</label>
-                    <select>
-                        <option>Select author</option>
+                    <select name='authorId' onChange={handleInputChange}>
                         {displayAuthors}
                     </select>
                 </div>
